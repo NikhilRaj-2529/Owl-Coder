8cc10bf6ab94444d1bd80f1925e97fb4b640d38d
Nikhil Raj <96733848+NikhilRaj-25@users.noreply.github.com>
2024-01-08 18:56:48 +0530

    Create Arithmetic Slices II - Subsequence.cpp

---
+++ Arithmetic Slices II - Subsequence.cpp
@@ -0,0 +1,20 @@
+vector<map<long long,long long>>vt;
+int ans=0;
+long long a,b,x;
+map<long long,long long>mpp;
+vt.push_back(mpp);
+for(int i=1;i<nums.size();i++){
+    vt.push_back(mpp);
+    for(int j=0;j<i;j++){
+        int c=1;
+        a=nums[i];
+        b=nums[j];
+        x=a-b;
+        if(vt[j][x]>0){
+            c+=vt[j][x];
+            ans+=vt[j][x];
+        }
+        vt[i][x]+=c;
+    }
+}
+return ans;
